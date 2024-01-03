a823628c60bb0db25c28fc081fd6a2ef443e23a7
Sergio Ramirez <120687376+Manson1945@users.noreply.github.com>
2024-01-03 20:33:04 -0300

    commit

--- JS/renderHeader.js
+++ JS/renderHeader.js
@@ -13,8 +13,13 @@ const renderHeader = ()=> {
         <a href="./index.html"><i class="fa-solid fa-house"></i>INICIO</a>
         <a href="../productos.html"><i class="fa-solid fa-shirt"></i>PRODUCTOS</a>
         <a href="#contact"><i class="fa-solid fa-phone-flip"></i>CONTACTO</a>
+        <a href="./carrito.html" class="headerBtn" > <i class="fa-solid fa-paper-plane"><span id="number">0</span></i> <span id="span">PEDIDO</span> </a>
     </nav>
             `
+
+            
+
+
 
         document.getElementById("header").innerHTML = salida;
 }
