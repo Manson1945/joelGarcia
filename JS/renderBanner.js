2fcf19f92fe58a2a64155e59844dc2a48aeb0674
Sergio Ramirez <120687376+Manson1945@users.noreply.github.com>
2024-01-17 10:34:48 -0300

    commit

--- JS/renderBanner.js
+++ JS/renderBanner.js
@@ -3,10 +3,13 @@
 const renderBanner = ()=> {
 
     const salida = `
-                <h2>¡LA MAYOR CALIDAD Y EL MENOR PRECIO, GARANTIZADO!</h2>
-                <h3>TELAS DE EXCELENTE CALIDAD</h3>
+                <h2>¡LA MAYOR CALIDAD Y EL MENOR PRECIO DEL PAIS, GARANTIZADO!</h2>
+                <h3>La mayor calidad de telas para remeras 
+                sublimables.</h3>
                 
-                <button><i class="fa-brands fa-whatsapp icon"></i>CONTACTANOS</button>
+                
                     `
                     document.getElementById("banner").innerHTML = salida;
 }
+
+/* <button><i class="fa-brands fa-whatsapp icon"></i>CONTACTANOS</button> */
